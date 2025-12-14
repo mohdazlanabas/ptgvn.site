f5f9c471ee33a1fdb3a4f082290179074d5ca837
Azlan <mohdazlan.abas@gamil.com>
2025-12-14 13:53:48 +0800

    Committed From Zsh At 01:53 PM, Sunday, December 14, 2025

--- wp-content/themes/azlan-theme/functions.php
+++ wp-content/themes/azlan-theme/functions.php
@@ -8,13 +8,8 @@ function unicorn() {
 
 add_action('after_setup_theme', 'unicorn');
 
-function mytheme_enqueue_styles() {
+function myfiles() {
     wp_enqueue_style('mytheme-style', get_stylesheet_uri());
-}
-
-add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
-
-function my_theme_enqueue_scripts() {
     wp_enqueue_script('main.js',
     get_template_directory_uri() . '/js/main.js',
     array('jquery'),
@@ -22,4 +17,4 @@ function my_theme_enqueue_scripts() {
     true);
 }
 
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'myfiles');

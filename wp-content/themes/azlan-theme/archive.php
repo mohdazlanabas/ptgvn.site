726586173af1e5d9d01a127bd32410961a32d928
Azlan <mohdazlan.abas@gamil.com>
2025-12-14 13:00:56 +0800

    Committed From Zsh At 01:00 PM, Sunday, December 14, 2025

---
+++ wp-content/themes/azlan-theme/archive.php
@@ -0,0 +1,19 @@
+<h1>Welcome to Azlan Theme</h1>
+
+<h2><?php the_archive_title(); ?></h2>
+
+<?php
+    while(have_posts()) {
+        the_post(); ?>
+        
+        <div>
+            <h3>
+                <a href="<?php the_permalink() ?>">
+                <?php the_title(); ?>
+                <?php the_content(); ?>
+            </h3>
+        </div>
+    <?php 
+    }
+?>
+<p> Foother</p>

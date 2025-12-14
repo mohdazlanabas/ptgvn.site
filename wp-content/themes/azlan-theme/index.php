a4a8bde910e2270b5fe39d7084edf01dba888a77
Azlan <mohdazlan.abas@gamil.com>
2025-12-14 12:46:26 +0800

    Committed From Zsh At 12:46 PM, Sunday, December 14, 2025

--- wp-content/themes/azlan-theme/index.php
+++ wp-content/themes/azlan-theme/index.php
@@ -3,11 +3,11 @@
 <?php
 
     while(have_posts()) {
-        the_post(); ?>
         <div>
             <h2>
                 <a href="<?php the_permalink() ?>">
                 <?php the_title(); ?>
+                <?php the_post(); ?>
                 <?php the_content(); ?>
             </h2>
         </div>

cea1c11cd7bd06cb6a4f34a092877635d3c3e66d
Azlan <mohdazlan.abas@gamil.com>
2025-12-14 15:34:31 +0800

    Committed From Zsh At 03:34 PM, Sunday, December 14, 2025

---
+++ wp-content/plugins/azlan-plugin/build/myplugin/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '30a3151e07021177ae74');
---
+++ wp-content/plugins/azlan-plugin/build/myplugin/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e5a5df189b669d1c89dd');

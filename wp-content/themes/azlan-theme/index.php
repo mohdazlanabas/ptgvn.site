f5abff54f3a41c193be29d460b57a9d5dcf64318
Azlan <mohdazlan.abas@gamil.com>
2025-12-14 12:32:29 +0800

    Committed From Zsh At 12:32 PM, Sunday, December 14, 2025

--- wp-content/themes/azlan-theme/index.php
+++ wp-content/themes/azlan-theme/index.php
@@ -4,10 +4,8 @@
 
     while(have_posts()) {
         the_post(); ?>
-        <div>
-            <h2><?php the_title(); ?></h2>
-            <div><?php the_content(); ?></div>
-        </div>
+        <div><h2><a href="#"><?php the_title(); ?></h2></div>
+        <div><?php the_content(); ?></div>
     <?php 
     }
 
